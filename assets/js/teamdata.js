ce3089a22a0de0f77f7da03b3dfdd43d3c5f5977
Utkarsh <suhasb.teamchimera@gmail.com>
2024-03-29 17:04:52 +0530

    updated 2025_batch

--- assets/js/teamdata.js
+++ assets/js/teamdata.js
@@ -497,6 +497,68 @@ var teamJs = new Vue({
 				name: "V Shashank Reddy",
 				subsystem: "Powertrain",
 			},
+		],
+		team2025: [
+			{
+				image: "assets/img/team/2025/Abdul Mannan Lalsha.jpg",
+				name: "Abdulmannan",
+				subsystem: "Project Manager, ESO",
+			},
+			{
+				image: "assets/img/team/2025/ankitha shet.jpg",
+				name: "Ankitha Shet",
+				subsystem: "Chassis",
+			},
+			{
+				image: "assets/img/team/2025/Aryan.jpeg",
+				name: "Aryan",
+				subsystem: "Mechanical Head(Dynamic), Accumulator",
+			},
+			{
+				image: "assets/img/team/2025/Channaveer.jpg",
+				name: "Channaveergauda Mali Patil",
+				subsystem: "Electrical LV Head, Safety Circuits",
+			},
+			{
+				image: "assets/img/team/2025/Chetan.jpg",
+				name: "Chetan",
+				subsystem: "Safety Circuits",
+			},
+			{
+				image: "assets/img/team/2025/Gaurav Makhija.jpg",
+				name: "Gaurav Makhija",
+				subsystem: "Team Captain, BMS",
+			},
+			{
+				image: "assets/img/team/2025/Shreyas R.jpg",
+				name: "Shreyas R",
+				subsystem: "DAS",
+			},
+			{
+				image: "assets/img/team/2025/Srinibash Panda.jpg",
+				name: "Srinibash Panda",
+				subsystem: "Mechanical Head(Static)",
+			},
+			{
+				image: "assets/img/team/2025/Utkarsh Goel.jpg",
+				name: "Utkarsh Goel",
+				subsystem: "Management Head, DAS",
+			},
+			{
+				image: "assets/img/team/2025/Varun R R.jpg",
+				name: "Varun R",
+				subsystem: "Electrical HV Head, Powertrain",
+			},
+			{
+				image: "assets/img/team/2025/Vimalesh Naram.jpg",
+				name: "VImalesh Naram",
+				subsystem: "Safety Circuits",
+			},
+			{
+				image: "assets/img/team/2025/Vishwaa Arumugam.jpeg",
+				name: "Vishwaa Arumugam",
+				subsystem: "Sponsorship Head",
+			},
 		]
 	},
 });
